ac2edac275066022c23a6249aa9e5598387752d6
filipepenati <filipepf15@gmail.com>
2018-04-03 16:21:20 -0300

    Primeiro commit

---
+++ script/teste1.R
@@ -0,0 +1,14 @@
+## LOAD PACKAGES ####
+library(dplyr)
+library(ggplot2)
+
+
+
+## READ IN DATA AND ORGANIZE ####
+### Read in data
+data("txhousing")
+
+## MAKE FIGURES ####
+
+## RUN DESCRIPTIVE STATISTICS ####
+
