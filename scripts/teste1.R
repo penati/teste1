425460f6b7aaf3b9308b269be08eab5771ee8085
filipepenati <filipepf15@gmail.com>
2018-04-09 23:47:22 -0300

    primeira analise stomrs

--- scripts/teste1.R
+++ scripts/teste1.R
@@ -3,15 +3,15 @@ library(dplyr)
 library(ggplot2)
 
 ## DATA PREPARATION ####
-
 data("txhousing")
 txh_data <- txhousing
 save(txh, file="data/txh_data.txt")
 
-
 ## READ IN DATA AND ORGANIZE ####
 ### Read in data
 data("data/txh_data.txt")
+
+
  
 ## MAKE FIGURES ####
 
